abac06e33d412034845e738e6e4e6e7c046065a9
Gaetan Bonnan <gaetan.bonnan@hotmail.fr>
2026-04-14 19:06:30 +0200

    add text of Comment venir

--- app/pages/2_Comment_venir.py
+++ app/pages/2_Comment_venir.py
@@ -18,3 +18,42 @@ gite_adresse = pd.DataFrame({
 })
 
 st.map(gite_adresse, zoom=12)
+
+# Lien Google Sheet
+
+st.markdown("""
+    ### [Google Sheet Train & Covoiturage](https://docs.google.com/spreadsheets/d/11nlYVrzULCtokI5QsRXU5TBGfkA1y9ruVCqx_SBTplg/edit?usp=sharing)
+
+""")
+
+# Train
+
+st.markdown("""
+    # Train
+""")
+
+col1, col2 = st.columns(2)
+
+col1.markdown("""
+## Aller
+
+Paris Montparnasse → La Loupe : TER (toutes les 30min jusqu'à 19h, ~24€) \n
+La Loupe → Gîte : navette (8min)
+""")
+
+col2.markdown("""
+## Retour
+
+Gite →  la gare de Verneuil-sur-Avre : navette (28min)  \n
+Verneuil-sur-Avre → Paris : TER (11h, 16h ou 18h, ~15€) \n
+⚠️ gare différente de l'aller ⚠️
+
+""")
+
+# Covoiturage
+
+st.markdown("""
+    # Covoiturage
+
+    1h40 depuis Porte d'Auteuil jusqu'au Gîte de l'Orée du Bois. \n
+""")
--- app/🎪_Arcadie.py
+++ app/🎪_Arcadie.py
@@ -1,5 +1,8 @@
 from utils import *
 
+# Affiche
+affiche = Image.open('data/affiche_arcadie.jpeg')
+st.image(affiche, use_container_width=False)
 
 st.set_page_config(
     page_title="Arcadie #3",
@@ -13,13 +16,5 @@ L’heure est trouble, la lumière décline entre les feuillages… en cette nui
 Après la Guinguette des Crevettes et le Bal des Étoiles, nous lançons une troisième incantation. Dans 47 jours, elle nous emportera loin du monde ordinaire, vers un refuge caché, là où la mousse est douce, les basses résonnent entre les arbres et les racines s’entrelacent 🌳
 
 Cette conversation sera notre grimoire commun. Vous y trouverez toutes les informations pratiques et autres sortilèges utiles à notre quête : se retrouver et faire vibrer la forêt.
-
-Et pour commencer
-Lieu : le gîte du château de Manou, proche Chartres (accessible en 1h30 de TER depuis Paris Montparnasse)
-Date : du 29 au 31 mai
-Teuf et activités : une dizaine de DJ, et bien d’autres surprises…
-Miam & drink : tout est prévu (repas et potions magiques)
-Refuge : chaque farfadet aura son oreiller
-Costumes : pour sélectionner vos plus beaux atours, écoutez les murmures de la forêt (tout ce qui brille, pousse, grimpe, fleurit est encouragé)
 
 On a super hâte ✨""")
