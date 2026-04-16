c71551f0492a4f317533f9fb374a5a074db6670e
Gaetan Bonnan <gaetan.bonnan@hotmail.fr>
2026-04-16 11:28:29 +0200

    Add streamlit_app.py for Cloud deployment

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -7,9 +7,9 @@ from utils import *
 # Définir les pages disponibles
 PAGES = {
     "🎪 Arcadie": "app/pages/1_🎪_Arcadie.py",
-    "🚗 Comment venir": "pages/4_🚗_Comment_venir.py",
-    "🎉 Programmation": "pages/2_🎉_Programmation.py",
-    "🎧 Line-up": "pages/3_🎧_Line-up.py"
+    "🚗 Comment venir": "app/pages/4_🚗_Comment_venir.py",
+    "🎉 Programmation": "app/pages/2_🎉_Programmation.py",
+    "🎧 Line-up": "app/pages/3_🎧_Line-up.py"
 }
 
 # CSS pour la barre de navigation horizontale scrollable
